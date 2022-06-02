00cb3f615ae5a7f1efa115b997996d54cad3bbc2
Claudeilton Dantas <fcd007@hotmail.com>
2022-06-01 22:16:22 -0300

    feat: init project

---
+++ src/main/java/com/bezkoder/springjwt/models/Role.java
@@ -0,0 +1,83 @@
+package com.bezkoder.springjwt.models;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Version;
+
+@Entity
+@Table(name = "roles")
+public class Role {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	@Enumerated(EnumType.STRING)
+	@Column(length = 20)
+	private ERole name;
+
+	@Column
+	private Date data_criacao;
+
+	@Column
+	private Date data_update;
+
+	@Version
+	private Integer version;
+
+	public Role() {
+
+	}
+
+	public Role(ERole name) {
+		this.name = name;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public ERole getName() {
+		return name;
+	}
+
+	public void setName(ERole name) {
+		this.name = name;
+	}
+
+	public Date getData_criacao() {
+		return data_criacao;
+	}
+
+	public void setData_criacao(Date data_criacao) {
+		this.data_criacao = data_criacao;
+	}
+
+	public Date getData_update() {
+		return data_update;
+	}
+
+	public void setData_update(Date data_update) {
+		this.data_update = data_update;
+	}
+
+	public Integer getVersion() {
+		return version;
+	}
+
+	public void setVersion(Integer version) {
+		this.version = version;
+	}
+
+}
---
+++ src/main/java/com/bezkoder/springjwt/models/User.java
@@ -0,0 +1,154 @@
+package com.bezkoder.springjwt.models;
+
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "users", uniqueConstraints = { @UniqueConstraint(columnNames = "username"),
+		@UniqueConstraint(columnNames = "email") })
+public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@NotBlank
+	@Size(max = 128)
+	private String username;
+
+	@NotBlank
+	@Size(max = 50)
+	@Email
+	private String email;
+
+	@Column
+	private Integer status;
+
+	@NotBlank
+	@Size(max = 120)
+	private String password;
+
+	@ManyToMany(fetch = FetchType.LAZY)
+	@JoinTable(name = "user_roles", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
+	private Set<Role> roles = new HashSet<>();
+
+	@Column
+	private Date data_criacao;
+
+	@Column
+	private Date data_update;
+
+	@Version
+	private Integer version;
+
+	public User() {
+	}
+
+	public User(String username, String email, String password) {
+		this.username = username;
+		this.email = email;
+		this.password = password;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Set<Role> getRoles() {
+		return roles;
+	}
+
+	public void setRoles(Set<Role> roles) {
+		this.roles = roles;
+	}
+
+	public Integer getStatus() {
+		return status;
+	}
+
+	public void setStatus(Integer status) {
+		this.status = status;
+	}
+
+	public Integer getVersion() {
+		return version;
+	}
+
+	public void setVersion(Integer version) {
+		this.version = version;
+	}
+
+	public Date getData_criacao() {
+		return data_criacao;
+	}
+
+	public void setData_criacao(Date data_criacao) {
+		this.data_criacao = data_criacao;
+	}
+
+	public Date getData_update() {
+		return data_update;
+	}
+
+	public void setData_update(Date data_update) {
+		this.data_update = data_update;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(email, id, roles, username, version);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		return Objects.equals(email, other.email) && Objects.equals(id, other.id) && Objects.equals(roles, other.roles)
+				&& Objects.equals(username, other.username) && Objects.equals(version, other.version);
+	}
+
+	@Override
+	public String toString() {
+		return id + ", username=" + username + ", email=" + email + ", roles=" + roles + ", versao=" + version;
+	}
+
+}
